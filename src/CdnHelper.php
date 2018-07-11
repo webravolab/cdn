2a359abcc8923274edd4e9115ac8937fc06607ec
Paolo Nardini <paolo.nardini@gmail.com>
2018-07-11 10:21:36 +0200

    Handle animated gif, bypassing any image processing (ignore resize / background / type change) - SImply copy the original file to the Cdn

--- src/CdnHelper.php
+++ src/CdnHelper.php
@@ -74,9 +74,27 @@ class CdnHelper implements CdnHelperInterface
         $param_name = null;
         $width = 0;
         $height = 0;
+        $is_animated_gif = false;
 
         try {
             extract($a_params, EXTR_OVERWRITE + EXTR_PREFIX_ALL, 'param');
+
+            $real_file_name = $this->checkImageFileExists($path);
+            if (empty($real_file_name)) {
+                // Image not found ... get fallback image
+                $fallback = $this->getConfiguration()['fallback_image'];
+                if (!empty($fallback)) {
+                    $real_file_name = $this->checkImageFileExists($fallback);
+                    if (empty($real_file_name)) {
+                        // Last chance ... use Image fallback
+                        $o_tmp = Image::create(100, 100);
+                        $real_file_name = $o_tmp->getFallback();
+                    }
+                }
+            }
+            // Check image last modified date and image extension
+            $real_file_modified_time = filemtime($real_file_name);
+            $real_extension = $this->getImageExtension($path);
 
             if (!empty($param_size)) {
                 $a_size = explode('x', $param_size);
@@ -118,48 +136,50 @@ class CdnHelper implements CdnHelperInterface
                     $param_type = strtolower($param_type);
                     break;
                 default:
-                    $a_parts = pathinfo($path);
-                    if (isset($a_parts['extension']) && in_array($a_parts['extension'], $this->_image_types)) {
-                        $param_type = $a_parts['extension'];
+                    if (empty($real_extension)) {
+                        // Force jpg for unknown image types
+                        $param_type = 'jpg';
                     }
                     else {
-                        $param_type = 'jpg';
+                        $param_type = $real_extension;
                     }
                     break;
             }
-            $real_file_name = $this->checkImageFileExists($path);
-            if (empty($real_file_name)) {
-                // Image not found ... get fallback image
-                $fallback = $this->getConfiguration()['fallback_image'];
-                if (!empty($fallback)) {
-                    $real_file_name = $this->checkImageFileExists($fallback);
-                    if (empty($real_file_name)) {
-                        // Last chance ... use Image fallback
-                        $o_tmp = Image::create(100, 100);
-                        $real_file_name = $o_tmp->getFallback();
+
+            if ($param_type == 'gif' || $real_extension == 'gif') {
+                // Check for animated gif
+                $is_animated_gif = $this->isGifAnimated($real_file_name);
+            }
+
+            $o_image = Image::open($real_file_name);
+
+            if (!$is_animated_gif) {
+                // ANIMATED GIF! Don't process with Gregwar/image and copy "as is"
+                // <TODO> Extract and process any single frame
+                // see https://stackoverflow.com/questions/718491/resize-animated-gif-file-without-destroying-animation
+
+                if ($width > 0 && $height > 0) {
+                    switch (strtolower($param_mode)) {
+                        case 'resize':
+                            $o_image->resize($width, $height);
+                            break;
+                        case 'zoom':
+                        case 'zoomcrop':
+                            $o_image->zoomCrop($width, $height, $bg);
+                            break;
+                        case 'crop':
+                        case 'cropresize':
+                            $o_image->cropResize($width, $height, $bg);
+                            break;
+                        case 'scale':
+                        case 'scaleresize':
+                            $o_image->scaleResize($width, $height, $bg);
+                            break;
                     }
                 }
-            }
-            $real_file_modified_time = filemtime($real_file_name);
-            $o_image = Image::open($real_file_name);
-            if ($width > 0 && $height > 0) {
-                switch ($param_mode) {
-                    case 'resize':
-                        $o_image->resize($width, $height);
-                        break;
-                    case 'zoom':
-                        $o_image->zoomCrop($width, $height, $bg);
-                        break;
-                    case 'crop':
-                        $o_image->cropResize($width, $height, $bg);
-                        break;
-                    case 'scale':
-                        $o_image->scaleResize($width, $height, $bg);
-                        break;
+                if (!empty($bg)) {
+                    $o_image->fillBackground($bg);
                 }
-            }
-            if (!empty($bg)) {
-                $o_image->fillBackground($bg);
             }
 
             $new_or_updated = true;
@@ -188,7 +208,17 @@ class CdnHelper implements CdnHelperInterface
                 $pretty_name = public_path($file_name);
                 if ($new_or_updated || !file_exists($pretty_name)) {
                     $cache_file_name = $o_image->cacheFile($param_type, $param_quality);
-                    $o_image->save($pretty_name, $param_type, $param_quality);
+                    if ($is_animated_gif) {
+                        // ANIMATED GIF! Don't process with Gregwar/image and copy "as is"
+                        // <TODO> Extract and process any single frame
+                        // see https://stackoverflow.com/questions/718491/resize-animated-gif-file-without-destroying-animation
+                        // Simply copy the file
+                        $this->assureDirectoryExists($pretty_name);
+                        copy($real_file_name, $pretty_name);
+                    }
+                    else {
+                        $o_image->save($pretty_name, $param_type, $param_quality);
+                    }
                     // Must be updated on CDN
                     $new_or_updated = true;
                 }
@@ -204,6 +234,10 @@ class CdnHelper implements CdnHelperInterface
         }
         catch (\Exception $e) {
             Log::error('CdnHelper / processImage Error:' . $e->getMessage());
+            $fallback = $this->getConfiguration()['fallback_image'];
+            if (!empty($fallback)) {
+                return $fallback;
+            }
             return 'error.jpg';
         }
     }
@@ -236,7 +270,7 @@ class CdnHelper implements CdnHelperInterface
     {
         try {
             $a_parts = pathinfo($path);
-            if (isset($a_parts['extension']) && in_array($a_parts['extension'], $this->_image_types)) {
+            if (isset($a_parts['extension']) && in_array(strtolower($a_parts['extension']), $this->_image_types)) {
                 $real_file_name = public_path($path);
                 if (file_exists($real_file_name)) {
                     return $real_file_name;
@@ -255,6 +289,21 @@ class CdnHelper implements CdnHelperInterface
         }
     }
 
+    protected function getImageExtension($file_name): ?string
+    {
+        try {
+            // Extract real image extension from path
+            $a_parts = pathinfo($file_name);
+            if (isset($a_parts['extension']) && in_array(strtolower($a_parts['extension']), $this->_image_types)) {
+                return strtolower($a_parts['extension']);
+            }
+            return null;
+        }
+        catch (\Exception $e) {
+            return null;
+        }
+    }
+
     protected function checkExtension($file_name, $extension) {
         $a_parts = pathinfo($file_name);
         if (isset($a_parts['extension']) && in_array($a_parts['extension'], $this->_image_types)) {
@@ -264,5 +313,39 @@ class CdnHelper implements CdnHelperInterface
             }
         }
         return $file_name;
+    }
+
+    protected function assureDirectoryExists($file_name)
+    {
+        $a_parts = parse_url($file_name);
+        if (isset($a_parts['path'])) {
+            $directory = public_path(dirname($a_parts['path']));
+            if (!is_dir($directory)) {
+                @mkdir($directory, 0777, true);
+            }
+        }
+    }
+
+    protected function isGifAnimated($file_name):int
+    {
+        // See: http://php.net/manual/en/function.imagecreatefromgif.php#104473
+        if(!($fh = @fopen($file_name, 'rb'))) {
+            return false;
+        }
+        $count = 0;
+
+        //an animated gif contains multiple "frames", with each frame having a
+        //header made up of:
+        // * a static 4-byte sequence (\x00\x21\xF9\x04)
+        // * 4 variable bytes
+        // * a static 2-byte sequence (\x00\x2C) (some variants may use \x00\x21 ?)
+
+        // We read through the file til we reach the end of the file, or we've found at least 2 frame headers
+        while(!feof($fh) && $count < 2) {
+            $chunk = fread($fh, 1024 * 100); //read 100kb at a time
+            $count += preg_match_all('#\x00\x21\xF9\x04.{4}\x00(\x2C|\x21)#s', $chunk, $matches);
+        }
+        fclose($fh);
+        return $count > 1;
     }
 }
